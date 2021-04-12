679299c263a663aeeba3d416280fd8098530fa43
diegoaguayo <dtaguayo@uc.cl>
2021-04-12 00:42:07 -0400

    change season model

--- app/models/season.rb
+++ app/models/season.rb
@@ -3,12 +3,6 @@ class Season
   include ActiveModel::Conversion
   extend ActiveModel::Naming
   
-  attr_accessor :name, :email, :content
-  
-  validates_presence_of :name
-  validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
-  validates_length_of :content, :maximum => 500
-  
   def initialize(season_n) 
     @episodes = episodes
   end
